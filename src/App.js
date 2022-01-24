67a71d75b5ee105948d1e99a93a9f92569c0cbe4
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-01-23 21:57:06 -0300

    tablet ready

--- src/App.js
+++ src/App.js
@@ -6,20 +6,24 @@ import Ecommerce from "./components/Ecommerce/Ecommerce";
 import Planning from "./components/Planning/Planning";
 import Social from "./components/Social/Social";
 import Shop from "./components/Shop/Shop";
+import Copyright from "./components/Copyright/Copyright";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/ecommerce" element={<Ecommerce />} />
-          <Route path="/planning" element={<Planning />} />
-          <Route path="/social" element={<Social />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/ecommerce" element={<Ecommerce />} />
+            <Route path="/planning" element={<Planning />} />
+            <Route path="/social" element={<Social />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </>
+      <Copyright />
     </div>
   );
 }
