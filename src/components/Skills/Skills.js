4942aa77eae985e5cf3efde557c45c5717f21ca2
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-04-17 19:36:34 -0400

    update

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -80,7 +80,7 @@ export default function Skills() {
           </div>
           <div className="skills-item">
             <img src={images.jest} alt="git" className="skills-icon" />
-            <p className="skill-name">Unit Testing with Jest</p>
+            <p className="skill-name">Unit Testing</p>
           </div>
         </div>
       </div>
